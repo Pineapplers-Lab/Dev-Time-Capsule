23fa3eb7206b5be52f6e98179b13e59b724fb6c4
ScoField <scofieldx911@gmail.com>
2025-12-19 03:27:44 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 set -e
 cd backend
 
-pip install --no-cache-dir -r requirements.txt
+# Install dependencies using python -m pip
+python -m pip install --no-cache-dir -r requirements.txt
 
-# Start the FastAPI app with uvicorn
+# Start FastAPI
 python -m uvicorn main:app --host 0.0.0.0 --port $PORT
